c002900c59f8b8b4f8d70c1e2104eb8c0319b97c
fescarvalho <fernando_scarvalho@hotmail.com>
2023-09-30 00:27:43 -0300

    feat - Created form and components to insert range of dates for search of data.

---
+++ src/Components/DataRange.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import DateInput from "./DateInput";
+
+const DataRange = () => {
+  const [inicio, setInicio] = React.useState("");
+  const [final, setFinal] = React.useState("");
+
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <DateInput
+        label="Inicio"
+        value={inicio}
+        onChange={({ target }) => setInicio(target.value)}
+      />
+
+      <DateInput
+        label="Final"
+        value={final}
+        onChange={({ target }) => setFinal(target.value)}
+      />
+    </form>
+  );
+};
+
+export default DataRange;
--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { useData } from "../Context/DataContext";
+import DataRange from "./DataRange";
 
 const Header = () => {
-  const { data } = useData();
-  console.log(data);
-  return <div></div>;
+  return (
+    <div>
+      <DataRange />
+    </div>
+  );
 };
 
 export default Header;
