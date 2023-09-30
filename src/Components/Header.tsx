12bcbc44c316ffb4772eeed42ea5c4f56b7a9641
fescarvalho <fernando_scarvalho@hotmail.com>
2023-09-30 02:36:03 -0300

    feat - Implemented navigation with react-router-dom]

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-
 import DataRange from "./DataRange";
 import Meses from "./Meses";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
   const [title, setTitle] = React.useState("Resumo");
+  const location = useLocation();
+
+  React.useEffect(() => {
+    if (location.pathname === "/") {
+      setTitle("Resumo");
+      document.title = "Fintech | Resumo";
+    } else if (location.pathname === "/vendas") {
+      setTitle("Vendas");
+      document.title = "Fintech | Vendas";
+    }
+  }, [location]);
+
   return (
     <header className="mb">
       <div className="daterange mb">
